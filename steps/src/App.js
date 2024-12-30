0f167df5e732d9db09fe614c241aafe2a2562efb
Zakyboss <Boochimo8@gmail.com>
2024-12-30 19:39:19 +0300

    Created Static display and about to integrate state

--- steps/src/App.js
+++ steps/src/App.js
@@ -1,10 +1,33 @@
-
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+const messages = [
+  'Learn React ⚛️',
+  'Apply for jobs 💼',
+  'Invest your new income 🤑',
+];
 function App() {
+  function  handlePrevious(){
+    // alert('Hello Previous')
+    
+  }
+  function handleNext(){
+
+  }
   return (
-    <div className="App">
-    <p style={{marginLeft:'45%', fontSize:'32px', padding:'2px', backgroundColor:'teal', marginRight:'38%', borderRadius:'20px', color:'white', fontFamily:'sans-serif'}}>Hello World!</p>
+    <div className="steps">
+   <div className='numbers'>
+ <div className={`${step>=1 ?'active':'' }`}>1</div>
+ <div className={`${step>=2 ?'active':'' }`}> 2</div>
+ <div className= {`${step>=3 ?'active':'' }`}> 3</div>
+   </div>
+   <p className='message'>Step {step}: {messages[step-1]} </p>
+   <div className='buttons'>
+<button style={{backgroundColor:'#7950f2',color:'white'}} onClick={handlePrevious}>Previous</button>
+<button style={{backgroundColor:'#7950f2',color:'white'}} onClick={handleNext}>Next</button>
+   </div>
+   
     </div>
+
   );
 }
 
