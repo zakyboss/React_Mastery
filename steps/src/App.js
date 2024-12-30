6dce14ac34346af74fb112eaec0aec153f60a6a9
Zakyboss <Boochimo8@gmail.com>
2024-12-30 22:29:56 +0300

    use state completed

--- steps/src/App.js
+++ steps/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 const messages = [
   'Learn React ⚛️',
@@ -6,19 +6,27 @@ const messages = [
   'Invest your new income 🤑',
 ];
 function App() {
+  const [step,setStep]= useState(1)
   function  handlePrevious(){
-    // alert('Hello Previous')
-    
+// In order to use state we have 3 parts 
+  //  add state 
+  //  use state 
+  //  update the state    
+ if( step>1 ){
+ setStep(step-1)
   }
+}
   function handleNext(){
-
+if( step<3){
+  setStep(step+1)
+   }
   }
   return (
     <div className="steps">
    <div className='numbers'>
- <div className={`${step>=1 ?'active':'' }`}>1</div>
- <div className={`${step>=2 ?'active':'' }`}> 2</div>
- <div className= {`${step>=3 ?'active':'' }`}> 3</div>
+ <div className={step>=1 ?'active':'' }>1</div>
+ <div className={step>=2 ?'active':'' }> 2</div>
+ <div className= {step>=3 ?'active':'' }> 3</div>
    </div>
    <p className='message'>Step {step}: {messages[step-1]} </p>
    <div className='buttons'>
