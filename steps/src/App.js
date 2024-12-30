f80d198979f8d1f9d8f8ef3d9f7cbce1c0680abf
Zakyboss <Boochimo8@gmail.com>
2024-12-30 15:43:31 +0300

    Setting up the steps which is  Project number 2

---
+++ steps/src/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="App">
+    <p style={{marginLeft:'45%', fontSize:'32px', padding:'2px', backgroundColor:'teal', marginRight:'38%', borderRadius:'20px', color:'white', fontFamily:'sans-serif'}}>Hello World!</p>
+    </div>
+  );
+}
+
+export default App;
