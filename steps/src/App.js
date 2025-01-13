6a063a6a31190aba7f04b954ff357c2c3ded86e9
Zakyboss <Boochimo8@gmail.com>
2025-01-13 17:33:36 +0300

    Working with close button

--- steps/src/App.js
+++ steps/src/App.js
@@ -6,6 +6,10 @@ const messages = [
 ];
 function App() {
   const [step,setStep]= useState(1)
+  const [display,setDisplay]= useState(false)
+  function handleClose(){
+setDisplay(!display)
+  }
   function  handlePrevious(){
 // In order to use state we have 3 parts 
   //  add state 
@@ -23,23 +27,31 @@ if( step<3){
   function handleReset(){
     setStep(1)
   }
-  return (
-    <div className="steps">
-   <div className='numbers'>
- <div className={step>=1 ?'active':'' }>1</div>
- <div className={step>=2 ?'active':'' }> 2</div>
- <div className={step>=3 ?'active':'' }> 3</div>
-   </div>
-   <p className='message'>Step {step}: {messages[step-1]} </p>
-   <div className='buttons'>
-<button style={{backgroundColor:'#7950f2',color:'white'}} onClick={handlePrevious}>Previous</button>
-<button style={{backgroundColor:'#7950f2',color:'white'}} onClick={handleNext}>Next</button>
-<button style={{backgroundColor:'Red',color:'white'}} onClick={handleReset}>Reset</button>
+  if(display){
+    return (
+      <div className="steps">
+     <div className='numbers'>
+   <div className={step>=1 ?'active':'' }>1</div>
+   <div className={step>=2 ?'active':'' }> 2</div>
+   <div className={step>=3 ?'active':'' }> 3</div>
+     </div>
+     <p className='message'>Step {step}: {messages[step-1]} </p>
+     <div className='buttons'>
+  <button style={{backgroundColor:'#7950f2',color:'white'}} onClick={handlePrevious}>Previous</button>
+  <button style={{backgroundColor:'#7950f2',color:'white'}} onClick={handleNext}>Next</button>
+  <button style={{backgroundColor:'Red',color:'white'}} onClick={handleReset}>Reset</button>
+  <button className='close' onClick={handleClose}>X</button>
+  </div>
   
-   </div>
+      </div>
+    );  
+  }else {
+    return (
+      <button className='close' onClick={handleClose} style={{fontSize:'18px'}}>x</button>
 
-    </div>
-  );  
+    )
+  }
+ 
 }
 
 export default App;
