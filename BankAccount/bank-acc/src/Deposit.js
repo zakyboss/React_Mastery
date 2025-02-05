ffad378973340e88cd515d9823cf5bcdf715d278
Zakyboss <Boochimo8@gmail.com>
2025-02-05 07:39:13 +0300

    Changed a button with fixed amount to input form

--- BankAccount/bank-acc/src/Deposit.js
+++ BankAccount/bank-acc/src/Deposit.js
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import "./index.css";
 
 export default function Deposit({ dispatch, active }) {
+  const [amount, setAmount] = useState("");
+
   return (
     <div>
+      <input
+        className="input"
+        type="number"
+        value={amount}
+        onChange={(e) => setAmount(() => e.target.value)}
+        placeholder="Enter Amount"
+      />
       <button
-        className="btn"
-        onClick={() => dispatch({ type: "deposit" })}
-        disabled={!active}
+        onClick={(e) => {
+          // e.preventDefault();
+          if (!amount || !active) {
+            return;
+          }
+          dispatch({ type: "deposit", payload: Number(amount) });
+          setAmount("");
+        }}
       >
-        Deposit 150
+        Deposit
       </button>
     </div>
   );
