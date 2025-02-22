77b8659d3cad325df156f0c09bc7b121d40e9125
Zakyboss <Boochimo8@gmail.com>
2025-02-22 10:05:30 +0300

    Customer features

---
+++ bankAccount-redux/bank-acc/src/features/customer/Customer.js
@@ -0,0 +1,8 @@
+import { useSelector } from "react-redux";
+
+function Customer() {
+  const customerFullName = useSelector((store) => store.customer.fullName);
+  return <h2>👋 Welcome,{customerFullName}</h2>;
+}
+
+export default Customer;
