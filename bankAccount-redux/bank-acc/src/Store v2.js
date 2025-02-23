8ef8651dccc6b02b83820dd3ae234ca6c6bec5db
Zakyboss <Boochimo8@gmail.com>
2025-02-23 15:37:02 +0300

    Progress

--- bankAccount-redux/bank-acc/src/Store v2.js
+++ bankAccount-redux/bank-acc/src/Store v2.js
@@ -1,10 +1,11 @@
-import { combineReducers, createStore } from "redux";
+import { applyMiddleware, combineReducers, createStore } from "redux";
 import accountReducer from "./features/account/accountSlice";
 import customerReducer from "./features/customer/customerSlice";
+import { thunk } from "redux-thunk";
 const rootReducer = combineReducers({
   account: accountReducer,
   customer: customerReducer,
 });
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
