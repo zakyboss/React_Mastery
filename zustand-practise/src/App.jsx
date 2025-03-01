8f4c03bb464ca351158256a108717bc0a1b1ad6a
Zakyboss <Boochimo8@gmail.com>
2025-03-01 07:53:40 +0300

    Almost done with blog logic

---
+++ zustand-practise/src/App.jsx
@@ -0,0 +1,14 @@
+import Form from "./components/Form";
+import Output from "./components/Output";
+import Outputs from "./components/Outputs";
+
+function App() {
+  return (
+    <div>
+      <Form />
+      <Outputs />
+    </div>
+  );
+}
+
+export default App;
